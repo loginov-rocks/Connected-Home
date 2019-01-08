293a60f8778d92814fe3f6b94e60922020b8d3fc
Danila Loginov <danila@loginov.rocks>
2019-01-09 02:11:36 +0300

    attach AM2320

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
+
+// Wi-Fi
+#include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
 #include <WiFiManager.h>
+
+// AM2320
+#include <Adafruit_Sensor.h>
+#include <Adafruit_AM2320.h>
 
 #define SERIAL_BAUDRATE 115200
 
 #define AUTOCONNECT_SSID "MQTT-Home-Monitor"
 #define AUTOCONNECT_PASSWORD "87654321"
+
+Adafruit_AM2320 am2320 = Adafruit_AM2320();
 
 void setup()
 {
@@ -15,15 +23,27 @@ void setup()
 
   // Wi-Fi connection setup.
   Serial.println(F("Establishing Wi-Fi connection..."));
-
   WiFiManager wifiManager;
   wifiManager.autoConnect(AUTOCONNECT_SSID, AUTOCONNECT_PASSWORD);
-
   Serial.println(F("Wi-Fi connection established"));
+
+  // AM2320 setup.
+  Serial.println(F("Setting up AM2320 sensor..."));
+  am2320.begin();
+  Serial.println(F("AM2320 sensor set up"));
+
+  // Print headline.
+  Serial.println(F("T, C\tH, %"));
 }
 
 void loop()
 {
-  Serial.println(F("Idle..."));
-  delay(1000);
+  float temperature = am2320.readTemperature();
+  float humidity = am2320.readHumidity();
+
+  Serial.print(temperature);
+  Serial.print(F("\t"));
+  Serial.println(humidity);
+
+  delay(2000);
 }
