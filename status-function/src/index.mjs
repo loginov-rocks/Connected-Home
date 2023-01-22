a541d9d05707d7a770b136711cf659985b1c6820
Danila Loginov <danila@loginov.rocks>
2023-01-21 19:29:01 -0800

    Status Lambda

--- status-function/src/index.mjs
+++ status-function/src/index.mjs
@@ -1,13 +1,23 @@
-import { TimestreamQueryClient, QueryCommand } from '@aws-sdk/client-timestream-query';
+const { TimestreamQueryClient, QueryCommand } = require('@aws-sdk/client-timestream-query');
 
-export const handler = async (event) => {
-    const client = new TimestreamQueryClient();
+const databaseName = process.env.DATABASE_NAME;
+const tableName = process.env.TABLE_NAME;
 
-    const command = new QueryCommand({
-        QueryString: 'SELECT * FROM myTestDb.myTestTable WHERE measure_name = \'temperature\' ORDER BY time DESC limit 1'
-    });
+const client = new TimestreamQueryClient();
 
+exports.handler = async (event) => {
+    console.log(JSON.stringify(event));
+
+    const params = {
+        QueryString: `SELECT * FROM "${databaseName}"."${tableName}" WHERE time > ago(1d) ORDER BY time DESC LIMIT 1`
+    };
+
+    console.log(JSON.stringify(params));
+
+    const command = new QueryCommand(params);
     const response = await client.send(command);
+
+    console.log(JSON.stringify(response));
 
     return response;
 };
