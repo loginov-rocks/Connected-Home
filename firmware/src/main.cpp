c3f18b558ad5d4689229e751cf351b60aa56a34d
Danila Loginov <danila@loginov.rocks>
2024-07-15 21:21:27 -0700

    Firmware: Refactor to send temperature and humidity using AwsIotWiFiClient

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,20 +1,23 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
+#include "Arduino.h"
 
-// Wi-Fi
-#include <ESP8266WiFi.h>
-#include <WiFiManager.h>
+#include "ArduinoJson.h"
+#include "AwsIotWiFiClient.h"
+#include "ESP8266WiFi.h"
 
-#include <AwsIotWiFiClient.h>
+// Uncomment the following to use the WiFiManager library.
+// #include "DNSServer.h"
+// #include "ESP8266WebServer.h"
+// #include "WiFiManager.h"
 
 // DHT11
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
+#include "Adafruit_Sensor.h"
+#include "DHT.h"
+#include "DHT_U.h"
 
-// Project headers
-#include "Config.h"
 #include "Secrets.h"
+
+const char ssid[] = "SSID";
+const char password[] = "Password";
 
 AwsIotWiFiClient awsIotWiFiClient;
 
@@ -24,14 +27,83 @@ BearSSL::PrivateKey clientPrivateKey(privateKeyFile);
 
 DHT_Unified dht(D4, DHT11);
 
+unsigned long previousMillis = 0;
+const long interval = 10000;
+
+void connectWiFi()
+{
+  Serial.print("Connecting to Wi-Fi \"");
+  Serial.print(ssid);
+  Serial.print("\"...");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print('.');
+  }
+
+  Serial.println();
+  Serial.print("Local IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Wi-Fi connection was successful!");
+}
+
+void publishMessage()
+{
+  Serial.println("Publishing message...");
+
+  // @see https://arduinojson.org/v6/how-to/determine-the-capacity-of-the-jsondocument/
+  DynamicJsonDocument json(128);
+
+  sensors_event_t event;
+
+  // Humidity
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println("Error reading humidity!");
+  }
+  else
+  {
+    json["humidity"] = event.relative_humidity;
+  }
+
+  // Temperature
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
+  {
+    Serial.println("Error reading temperature!");
+  }
+  else
+  {
+    json["temperature"] = event.temperature;
+  }
+
+  // Timestamp
+  time_t now = time(nullptr);
+  json["timestamp"] = now;
+
+  char message[128];
+  serializeJson(json, message);
+
+  awsIotWiFiClient.publishMessage(publishTopicName, message);
+
+  Serial.print("Message successfully published: ");
+  Serial.println(message);
+}
+
 void receiveMessage(char *topic, byte *payload, unsigned int length)
 {
-  Serial.println("Some message received");
+  Serial.print("Message received: ");
+  Serial.write(payload, length);
+  Serial.println();
 }
 
 void setupAwsIotWiFiClient()
 {
-  Serial.println("Setup AWS IoT Wi-Fi Client...");
+  Serial.println("Setting up AWS IoT Wi-Fi Client...");
 
   awsIotWiFiClient.setDebugOutput(true)
       .setCertificates(&trustAnchorCertificate, &clientCertificate, &clientPrivateKey)
@@ -48,69 +120,38 @@ void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
 
-  Serial.begin(SERIAL_BAUDRATE);
+  Serial.begin(9600);
+  Serial.println("Setup...");
 
-  // Wi-Fi connection setup.
-  Serial.println(F("Establishing Wi-Fi connection..."));
-  WiFiManager wifiManager;
-  wifiManager.autoConnect(AUTOCONNECT_SSID, AUTOCONNECT_PASSWORD);
-  Serial.println(F("Wi-Fi connection established"));
+  connectWiFi();
+  // Comment the previous line and uncomment the following to use the WiFiManager library.
+  // WiFiManager wifiManager;
+  // wifiManager.autoConnect("Connected-Home");
+  // Serial.println("Connected!");
 
   setupAwsIotWiFiClient();
 
-  // DHT11 setup.
-  Serial.println(F("Setting up DHT11 sensor..."));
+  // DHT11
+  Serial.println("Setting up DHT11 sensor...");
   dht.begin();
-  Serial.println(F("DHT11 sensor set up"));
+  Serial.println("DHT11 sensor setup was successful!");
+
+  Serial.println("Setup was successful!");
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis > previousMillis + interval)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    previousMillis = currentMillis;
+    publishMessage();
+
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
   awsIotWiFiClient.loop();
-
-  // @see https://arduinojson.org/v6/how-to/determine-the-capacity-of-the-jsondocument/
-  DynamicJsonDocument json(1024);
-  // TODO: Generate UUID on setup.
-  json["sessionId"] = "1-2-3-4";
-  json["millis"] = millis();
-
-  struct tm timeinfo;
-  time_t now = time(nullptr);
-  gmtime_r(&now, &timeinfo);
-  json["time"] = asctime(&timeinfo);
-
-  sensors_event_t event;
-
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature))
-  {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else
-  {
-    json["temperature"] = event.temperature;
-  }
-
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity))
-  {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else
-  {
-    json["humidity"] = event.relative_humidity;
-  }
-
-  char message[1024];
-  serializeJson(json, message);
-
-  awsIotWiFiClient.publishMessage(publishTopicName, message);
-
-  Serial.println(message);
-
-  digitalWrite(LED_BUILTIN, LOW);
-
-  delay(LOOP_DELAY);
 }
